df628d30ae74f18923c2422705de6b77faa61ccf
office_v1 <961909318@qq.com>
2025-09-14 20:21:15 +0800

    Changes

---
+++ dose/show.sql
@@ -0,0 +1,21 @@
+show databases ;
+create database 2302a;
+use 2302a;
+CREATE TABLE dept (
+                      deptno INT,
+                      dname STRING,
+                      loc INT
+)
+-- 指定存储格式为TextFile（默认存储格式），数据按行存储，字段间默认用制表符(\t)分隔
+    ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY '\t'
+    STORED AS TEXTFILE;
+
+INSERT INTO dept (deptno, dname, loc) VALUES
+                                          (10, 'AcCOUNTING', 1700),
+                                          (20, 'RESEARCH', 1800),
+                                          (30, 'SALES', 1900),
+                                          (40, 'OPERATIONS', 1700);
+
+select * from dept;
+select * from dept;
